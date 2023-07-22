4b2553576302be6fb3fd98852091a3dfc2998a3c
Marco Toniut <marco.toniut@gmail.com>
2023-07-22 22:23:12 +0100

    tutorial: system sets

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -6,12 +6,31 @@ pub mod components;
 pub mod resources;
 pub mod systems;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub enum PlayerSystemSet {
+//     Movement,
+//     Confinement
+// }
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
-            .add_systems(Update, (player_movement, confine_player_movement).chain())
+        app.configure_set(Update, MovementSystemSet.before(ConfinementSystemSet))
+            .add_systems(Startup, spawn_player)
+            .add_systems(
+                Update,
+                (
+                    player_movement.in_set(MovementSystemSet),
+                    confine_player_movement.in_set(ConfinementSystemSet),
+                ),
+            )
             .add_systems(Update, player_hit_star)
             .add_systems(Update, enemy_hit_player);
     }
