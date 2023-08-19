6d6ba15c2345950b06b392287ebc90a16240116a
Marco Toniut <marco.toniut@gmail.com>
2023-08-19 22:05:23 +0100

    remove old modules

--- src/game/player/components.rs
+++
@@ -1,7 +0,0 @@
-use bevy::prelude::*;
-
-#[derive(Component)]
-pub struct Player {}
-
-pub const PLAYER_SPEED: f32 = 500.0;
-pub const PLAYER_SIZE: f32 = 64.0;
--- src/game/star/components.rs
+++
@@ -1,7 +0,0 @@
-use bevy::prelude::*;
-
-#[derive(Component)]
-pub struct Star {}
-
-pub const STAR_SIZE: f32 = 60.0;
-pub const NUMBER_OF_STARS: usize = 10;
