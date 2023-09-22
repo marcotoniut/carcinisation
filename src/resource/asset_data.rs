9a3f365a745420d7542f379aa06323fa3b0b5c8d
Marco Toniut <marco.toniut@gmail.com>
2023-09-22 12:31:00 +0100

    simplify asset data generation

--- src/resource/asset_data.rs
+++ src/resource/asset_data.rs
@@ -5,38 +5,32 @@ use crate::stage::data::StageSpawn;
 use crate::stage::data::StageStep;
 use serde::Deserialize;
 
-type CallStageSpawns = fn() -> Vec<StageSpawn>;
-type CallStageSteps = fn() -> Vec<StageStep>;
-
 #[derive(Deserialize, Debug, Clone)]
 pub struct SkyboxData {
     pub path: String,
     pub frames: usize,
 }
 
+// TODO could probably do without this struct, replica of StageData
 pub struct AssetData {
     pub name: String,
     pub background: String,
     pub skybox: SkyboxData,
     pub start_coordinates: Option<Vec2>,
-    pub _get_spawns: CallStageSpawns,
-    pub _get_steps: CallStageSteps,
+    pub spawns: Vec<StageSpawn>,
+    pub steps: Vec<StageStep>,
 }
 
 impl AssetData {
     pub fn get_data(&self) -> StageData {
-        let spawns = (self._get_spawns)();
-        let steps = (self._get_steps)();
-
-        let stage_data = StageData {
+        StageData {
             name: self.name.to_string(),
             background: self.background.to_string(),
             skybox: Some(self.skybox.clone()),
             start_coordinates: self.start_coordinates,
-            spawns,
-            steps,
-        };
-        return stage_data;
+            spawns: self.spawns.clone(),
+            steps: self.steps.clone(),
+        }
     }
 }
 
--- src/resource/asteroid.rs
+++ src/resource/asteroid.rs
@@ -16,68 +16,18 @@ lazy_static! {
             frames: 1,
         },
         start_coordinates: Some(Vec2::new(0.0, 0.0)),
-        _get_spawns,
-        _get_steps,
+        spawns: make_spawns(),
+        steps: make_steps(),
     };
 }
 
-pub fn _get_spawns() -> Vec<StageSpawn> {
-    let mut spawns = Vec::new();
-
-    spawns.push(StageSpawn::Destructible(DestructibleSpawn {
-        destructible_type: DestructibleType::Lamp,
-        coordinates: Vec2 { x: 30.0, y: 0.0 },
-        elapsed: 0.0,
-        contains: Some(Box::new(ContainerSpawn::Enemy(EnemySpawn {
-            enemy_type: EnemyType::Mosquito,
-            coordinates: Vec2 { x: 60.0, y: 100.0 },
-            base_speed: 5.0,
-            elapsed: 1.4,
-            steps: vec![
-                EnemyStep::Movement {
-                    coordinates: Vec2 { x: 50.0, y: 0.0 },
-                    attacking: true,
-                    speed: 5.0,
-                },
-                EnemyStep::Stop { duration: 1.0 },
-                EnemyStep::Attack { duration: 1.0 },
-                EnemyStep::Movement {
-                    coordinates: Vec2 { x: 10.0, y: 0.0 },
-                    attacking: true,
-                    speed: 3.0,
-                },
-                EnemyStep::CircleAround { duration: 4.0 },
-            ],
-            contains: None,
-        }))),
-    }));
-    spawns.push(StageSpawn::Destructible(DestructibleSpawn {
-        destructible_type: DestructibleType::Window,
-        coordinates: Vec2 { x: 20.0, y: 0.0 },
-        elapsed: 0.0,
-        contains: Some(Box::new(ContainerSpawn::Powerup(PowerupSpawn {
-            powerup_type: PowerupType::BigHealthpack,
-            coordinates: Vec2 { x: 30.0, y: 10.0 },
+pub fn make_spawns() -> Vec<StageSpawn> {
+    vec![
+        StageSpawn::Destructible(DestructibleSpawn {
+            destructible_type: DestructibleType::Lamp,
+            coordinates: Vec2 { x: 30.0, y: 0.0 },
             elapsed: 0.0,
-        }))),
-    }));
-    spawns.push(StageSpawn::Powerup(PowerupSpawn {
-        powerup_type: PowerupType::BigHealthpack,
-        coordinates: Vec2 { x: 30.0, y: 10.0 },
-        elapsed: 0.0,
-    }));
-
-    return spawns;
-}
-
-pub fn _get_steps() -> Vec<StageStep> {
-    let mut steps = Vec::new();
-
-    steps.push(StageStep::Movement {
-        coordinates: Vec2 { x: 50.0, y: 0.0 },
-        base_speed: 10.0,
-        spawns: vec![
-            StageSpawn::Enemy(EnemySpawn {
+            contains: Some(Box::new(ContainerSpawn::Enemy(EnemySpawn {
                 enemy_type: EnemyType::Mosquito,
                 coordinates: Vec2 { x: 60.0, y: 100.0 },
                 base_speed: 5.0,
@@ -98,61 +48,105 @@ pub fn _get_steps() -> Vec<StageStep> {
                     EnemyStep::CircleAround { duration: 4.0 },
                 ],
                 contains: None,
-            }),
-            StageSpawn::Enemy(EnemySpawn {
-                enemy_type: EnemyType::Mosquito,
-                coordinates: Vec2 { x: 120.0, y: 100.0 },
-                base_speed: 5.0,
-                elapsed: 0.4,
-                steps: vec![],
-                contains: None,
-            }),
-            StageSpawn::Enemy(EnemySpawn {
-                enemy_type: EnemyType::Mosquito,
-                coordinates: Vec2 { x: 130.0, y: 70.0 },
-                base_speed: 5.0,
-                elapsed: 2.3,
-                steps: vec![],
-                contains: None,
-            }),
-        ],
-    });
-    steps.push(StageStep::Stop {
-        resume_conditions: Some(vec![StageActionResumeCondition::KillAll]),
-        max_duration: Some(3),
-        spawns: vec![StageSpawn::Enemy(EnemySpawn {
-            enemy_type: EnemyType::Mosquito,
+            }))),
+        }),
+        StageSpawn::Destructible(DestructibleSpawn {
+            destructible_type: DestructibleType::Window,
             coordinates: Vec2 { x: 20.0, y: 0.0 },
-            base_speed: 5.0,
-            elapsed: 2.4,
-            steps: vec![],
-            contains: None,
-        })],
-    });
-    steps.push(StageStep::Movement {
-        coordinates: Vec2 { x: 34.0, y: 62.0 },
-        base_speed: 8.0,
-        spawns: vec![],
-    });
-    steps.push(
+            elapsed: 0.0,
+            contains: Some(Box::new(ContainerSpawn::Powerup(PowerupSpawn {
+                powerup_type: PowerupType::BigHealthpack,
+                coordinates: Vec2 { x: 30.0, y: 10.0 },
+                elapsed: 0.0,
+            }))),
+        }),
+        StageSpawn::Powerup(PowerupSpawn {
+            powerup_type: PowerupType::BigHealthpack,
+            coordinates: Vec2 { x: 30.0, y: 10.0 },
+            elapsed: 0.0,
+        }),
+    ]
+}
+
+pub fn make_steps() -> Vec<StageStep> {
+    vec![
+        StageStep::Movement {
+            coordinates: Vec2 { x: 50.0, y: 0.0 },
+            base_speed: 10.0,
+            spawns: vec![
+                StageSpawn::Enemy(EnemySpawn {
+                    enemy_type: EnemyType::Mosquito,
+                    coordinates: Vec2 { x: 60.0, y: 100.0 },
+                    base_speed: 5.0,
+                    elapsed: 1.4,
+                    steps: vec![
+                        EnemyStep::Movement {
+                            coordinates: Vec2 { x: 50.0, y: 0.0 },
+                            attacking: true,
+                            speed: 5.0,
+                        },
+                        EnemyStep::Stop { duration: 1.0 },
+                        EnemyStep::Attack { duration: 1.0 },
+                        EnemyStep::Movement {
+                            coordinates: Vec2 { x: 10.0, y: 0.0 },
+                            attacking: true,
+                            speed: 3.0,
+                        },
+                        EnemyStep::CircleAround { duration: 4.0 },
+                    ],
+                    contains: None,
+                }),
+                StageSpawn::Enemy(EnemySpawn {
+                    enemy_type: EnemyType::Mosquito,
+                    coordinates: Vec2 { x: 120.0, y: 100.0 },
+                    base_speed: 5.0,
+                    elapsed: 0.4,
+                    steps: vec![],
+                    contains: None,
+                }),
+                StageSpawn::Enemy(EnemySpawn {
+                    enemy_type: EnemyType::Mosquito,
+                    coordinates: Vec2 { x: 130.0, y: 70.0 },
+                    base_speed: 5.0,
+                    elapsed: 2.3,
+                    steps: vec![],
+                    contains: None,
+                }),
+            ],
+        },
+        StageStep::Stop {
+            resume_conditions: Some(vec![StageActionResumeCondition::KillAll]),
+            max_duration: Some(3),
+            spawns: vec![StageSpawn::Enemy(EnemySpawn {
+                enemy_type: EnemyType::Mosquito,
+                coordinates: Vec2 { x: 20.0, y: 0.0 },
+                base_speed: 5.0,
+                elapsed: 2.4,
+                steps: vec![],
+                contains: None,
+            })],
+        },
+        StageStep::Movement {
+            coordinates: Vec2 { x: 34.0, y: 62.0 },
+            base_speed: 8.0,
+            spawns: vec![],
+        },
         //TODO
         StageStep::Stop {
             resume_conditions: Some(vec![]),
             max_duration: Some(4),
             spawns: vec![],
         },
-    );
-    steps.push(StageStep::Movement {
-        coordinates: Vec2 { x: 90.0, y: 0.0 },
-        base_speed: 4.0,
-        spawns: vec![],
-    });
-    steps.push(StageStep::Movement {
-        coordinates: Vec2 { x: 0.0, y: 0.0 },
-        base_speed: 2.0,
-        spawns: vec![],
-    });
-    steps.push(
+        StageStep::Movement {
+            coordinates: Vec2 { x: 90.0, y: 0.0 },
+            base_speed: 4.0,
+            spawns: vec![],
+        },
+        StageStep::Movement {
+            coordinates: Vec2 { x: 0.0, y: 0.0 },
+            base_speed: 2.0,
+            spawns: vec![],
+        },
         //TODO
         StageStep::Stop {
             resume_conditions: Some(vec![
@@ -162,7 +156,5 @@ pub fn _get_steps() -> Vec<StageStep> {
             max_duration: None,
             spawns: vec![],
         },
-    );
-
-    return steps;
+    ]
 }
