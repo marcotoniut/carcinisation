8cc984e8ba400c2134eb35bf46eb36936f96bce3
Marco Toniut <marco.toniut@gmail.com>
2023-09-24 15:05:33 +0100

    fix audio

--- src/systems/spawn.rs
+++ src/systems/spawn.rs
@@ -1,14 +1,23 @@
-use bevy::{audio::PlaybackMode, prelude::*};
+use bevy::{
+    audio::{PlaybackMode, Volume},
+    prelude::*,
+};
 
-use super::audio::{AudioSystemBundle, AudioSystemType};
+use super::audio::{AudioSystemBundle, AudioSystemType, VolumeSettings};
 
-pub fn spawn_music(commands: &mut Commands, asset_server: &Res<AssetServer>, music_path: String) {
+pub fn spawn_music(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    volume_settings: Res<VolumeSettings>,
+    music_path: String,
+) {
     let sound_effect = asset_server.load(music_path);
     commands.spawn((
         AudioBundle {
             source: sound_effect,
             settings: PlaybackSettings {
                 mode: PlaybackMode::Loop,
+                volume: Volume::new_relative(volume_settings.1 * 1.0),
                 ..default()
             },
             ..default()
