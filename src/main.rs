ce5860d89d5d80a20ea3088912dd166bece7bf01
Marco Toniut <marco.toniut@gmail.com>
2023-09-25 10:44:37 +0100

    disable debug keys

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use globals::{DEFAULT_CROSSHAIR_INDEX, SCREEN_RESOLUTION};
 use leafwing_input_manager::{prelude::InputManagerPlugin, Actionlike};
 use seldom_pixel::prelude::*;
 use stage::{player::crosshair::CrosshairSettings, StagePlugin};
-use systems::{audio::VolumeSettings, camera::move_camera, *};
+use systems::{audio::VolumeSettings, camera::*, *};
 // use transitions::spiral::TransitionVenetianPlugin;
 
 fn main() {
@@ -84,15 +84,19 @@ fn main() {
     // .add_plugins(MainMenuPlugin)
     .add_plugins(InputManagerPlugin::<GBInput>::default())
     .add_systems(Startup, (set_framespace, spawn_camera, spawn_gb_input))
-    .add_systems(Update, move_camera)
-    .add_systems(Update, input_exit_game)
     .add_systems(Update, input_snd_menu)
     // TODO should this be placed at main?
     // .add_systems(Update, handle_game_over)
-    .add_systems(
-        Update,
-        (transition_to_game_state, transition_to_main_menu_state),
-    )
+    // DEBUG
+    // .add_systems(
+    //     Update,
+    //     (
+    //         move_camera,
+    //         transition_to_game_state,
+    //         transition_to_main_menu_state,
+    //         input_exit_game,
+    //     ),
+    // )
     .run();
 }
 
@@ -108,13 +112,13 @@ pub enum GBInput {
     Start,
     Select,
     // debug inputs
-    DUp,
-    DDown,
-    DLeft,
-    DRight,
-    DToGame,
-    DToMainMenu,
-    DExit,
+    // DUp,
+    // DDown,
+    // DLeft,
+    // DRight,
+    // DToGame,
+    // DToMainMenu,
+    // DExit,
 }
 
 #[derive(States, Debug, Clone, Eq, PartialEq, Hash, Default)]
