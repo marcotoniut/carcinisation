fb2a21f2c8eaa4280b319064807222f1e4e0b493
Marco Toniut <marco.toniut@gmail.com>
2023-09-27 10:02:44 +0100

    extract MovementVec2Position to plugins.movement.structs

--- src/plugins/movement/mod.rs
+++ src/plugins/movement/mod.rs
@@ -1,1 +1,2 @@
 pub mod pursue;
+pub mod structs;
--- src/plugins/movement/pursue/systems.rs
+++ src/plugins/movement/pursue/systems.rs
@@ -1,14 +1,14 @@
 use bevy::prelude::*;
 
-use crate::core::time::DeltaTime;
+use crate::{core::time::DeltaTime, plugins::movement::structs::MovementVec2Position};
 
 use super::components::*;
 
 /** TODO generalise current position via a generic LinearPosition trait */
-pub fn update<T: DeltaTime + 'static + Resource, P: Pursue + 'static + Component>(
+pub fn update<T: DeltaTime + 'static + Resource, P: MovementVec2Position + Component>(
     mut movement_query: Query<
-        (&mut P, &PursueSpeed<T>, &PursueTargetPosition<T>),
-        Without<PursueTargetReached<T>>,
+        (&mut P, &PursueSpeed<T, P>, &PursueTargetPosition<T, P>),
+        Without<PursueTargetReached<T, P>>,
     >,
     delta_time: Res<T>,
 ) {
@@ -21,11 +21,27 @@ pub fn update<T: DeltaTime + 'static + Resource, P: Pursue + 'static + Component
     }
 }
 
-pub fn check_x_reached<T: DeltaTime + 'static + Resource, P: Pursue + 'static + Component>(
+/**
+ * What to do if there's already a bundle? Should I simply clean it up on added?
+ */
+pub fn on_position_added<T: DeltaTime + 'static + Resource, P: MovementVec2Position + Component>(
+    mut commands: Commands,
+    mut movement_query: Query<(Entity, Added<PursueTargetPosition<T, P>>)>,
+) {
+    for (entity, _) in &mut movement_query.iter_mut() {
+        commands
+            .entity(entity)
+            .remove::<PursueTargetXReached<T, P>>()
+            .remove::<PursueTargetYReached<T, P>>()
+            .remove::<PursueTargetReached<T, P>>();
+    }
+}
+
+pub fn check_x_reached<T: DeltaTime + 'static + Resource, P: MovementVec2Position + Component>(
     mut commands: Commands,
     mut movement_query: Query<
-        (Entity, &P, &PursueSpeed<T>, &PursueTargetPosition<T>),
-        Without<PursueTargetXReached<T>>,
+        (Entity, &P, &PursueSpeed<T, P>, &PursueTargetPosition<T, P>),
+        Without<PursueTargetXReached<T, P>>,
     >,
 ) {
     for (entity, position, speed, target) in &mut movement_query.iter_mut() {
@@ -35,16 +51,16 @@ pub fn check_x_reached<T: DeltaTime + 'static + Resource, P: Pursue + 'static + 
         {
             commands
                 .entity(entity)
-                .insert(PursueTargetXReached::<T>::new());
+                .insert(PursueTargetXReached::<T, P>::new());
         }
     }
 }
 
-pub fn check_y_reached<T: DeltaTime + 'static + Resource, P: Pursue + 'static + Component>(
+pub fn check_y_reached<T: DeltaTime + 'static + Resource, P: MovementVec2Position + Component>(
     mut commands: Commands,
     mut movement_query: Query<
-        (Entity, &P, &PursueSpeed<T>, &PursueTargetPosition<T>),
-        Without<PursueTargetYReached<T>>,
+        (Entity, &P, &PursueSpeed<T, P>, &PursueTargetPosition<T, P>),
+        Without<PursueTargetYReached<T, P>>,
     >,
 ) {
     for (entity, position, speed, target) in &mut movement_query.iter_mut() {
@@ -54,20 +70,20 @@ pub fn check_y_reached<T: DeltaTime + 'static + Resource, P: Pursue + 'static + 
         {
             commands
                 .entity(entity)
-                .insert(PursueTargetYReached::<T>::new());
+                .insert(PursueTargetYReached::<T, P>::new());
         }
     }
 }
 
 // TODO, could be using the other checks at the same time to avoid a next frame "Reached" status
-pub fn check_reached<T: DeltaTime + 'static + Resource>(
+pub fn check_reached<T: DeltaTime + 'static + Resource, P: MovementVec2Position>(
     mut commands: Commands,
     mut movement_query: Query<
         Entity,
         (
-            With<PursueTargetXReached<T>>,
-            With<PursueTargetYReached<T>>,
-            Without<PursueTargetReached<T>>,
+            With<PursueTargetXReached<T, P>>,
+            With<PursueTargetYReached<T, P>>,
+            Without<PursueTargetReached<T, P>>,
         ),
     >,
 ) {
@@ -75,7 +91,7 @@ pub fn check_reached<T: DeltaTime + 'static + Resource>(
         {
             commands
                 .entity(entity)
-                .insert(PursueTargetReached::<T>::new());
+                .insert(PursueTargetReached::<T, P>::new());
         }
     }
 }
