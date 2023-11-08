dafd74f303f2b83b2539069b4eed5fbaa4d00a01
Marco Toniut <marco.toniut@gmail.com>
2023-11-08 01:42:54 +0000

    fix game progression

--- src/cutscene/systems/setup.rs
+++ src/cutscene/systems/setup.rs
@@ -9,7 +9,6 @@ use crate::{
     globals::mark_for_despawn_by_component_query,
 };
 use bevy::prelude::*;
-use seldom_pixel::prelude::PxSubPosition;
 
 pub fn on_startup(
     mut commands: Commands,
@@ -24,21 +23,20 @@ pub fn on_startup(
         commands.insert_resource::<CutsceneData>(data.clone());
         commands.insert_resource::<CutsceneProgress>(CutsceneProgress { index: 0 });
 
-        commands.spawn((
-            Cinematic,
-            Name::new("Cutscene"),
-            PxSubPosition(Vec2::new(50., 30.)),
-        ));
+        commands.spawn((Cinematic, Name::new("Cutscene")));
     }
 }
 
 pub fn on_shutdown(
     mut commands: Commands,
     mut event_reader: EventReader<CutsceneShutdownEvent>,
+    mut cutscene_state_next_state: ResMut<NextState<CutscenePluginUpdateState>>,
     cinematic_query: Query<Entity, With<Cinematic>>,
     cutscene_entity_query: Query<Entity, With<CutsceneEntity>>,
 ) {
     for _ in event_reader.iter() {
+        cutscene_state_next_state.set(CutscenePluginUpdateState::Inactive);
+
         mark_for_despawn_by_component_query(&mut commands, &cutscene_entity_query);
         mark_for_despawn_by_component_query(&mut commands, &cinematic_query);
     }
