1d89c137ba7c920e809a59adf8c91437104eb4c3
Marco Toniut <marco.toniut@gmail.com>
2024-06-23 12:46:41 +0100

    feat(editor): mouse movement

--- tools/editor/src/constants.rs
+++ tools/editor/src/constants.rs
@@ -1,1 +1,7 @@
 pub const ASSETS_PATH: &str = "../../assets/";
+
+pub const CAMERA_MOVE_SENSITIVITY: f32 = 1.0;
+pub const CAMERA_ZOOM_SPEED: f32 = 0.1;
+pub const CAMERA_ZOOM_MIN: f32 = 0.5;
+pub const CAMERA_ZOOM_MAX: f32 = 2.0;
+pub const CAMERA_MOVE_BOUNDARY: f32 = 2000.0;
