ea7d631158ce9bb13e1f25e08d013d066a60676d
Marco Toniut <marco.toniut@gmail.com>
2024-07-10 17:47:27 +0100

    chore: reify PickupDropSpawn and EnemyDropSpawn

--- src/stage/systems/spawn.rs
+++ src/stage/systems/spawn.rs
@@ -1,6 +1,7 @@
 use crate::stage::{
     components::{
         interactive::{Collider, ColliderData, Dead},
+        placement::Depth,
         SpawnDrop, StageEntity,
     },
     data::ContainerSpawn,
@@ -271,18 +272,21 @@ pub fn check_dead_drop(
     mut commands: Commands,
     mut assets_sprite: PxAssets<PxSprite>,
     mut attack_query: Query<&mut UnhittableList, With<PlayerAttack>>,
-    query: Query<(&SpawnDrop, &PxSubPosition), Added<Dead>>,
+    query: Query<(&SpawnDrop, &PxSubPosition, &Depth), Added<Dead>>,
 ) {
-    for (spawn_drop, position) in &mut query.iter() {
+    for (spawn_drop, position, depth) in &mut query.iter() {
         let entity = match spawn_drop.contains.clone() {
-            ContainerSpawn::Pickup(mut spawn) => {
-                spawn.coordinates = position.0;
-                spawn_pickup(&mut commands, &mut assets_sprite, Vec2::ZERO, &spawn)
-            }
-            ContainerSpawn::Enemy(mut spawn) => {
-                spawn.coordinates = position.0;
-                spawn_enemy(&mut commands, Vec2::ZERO, &spawn)
-            }
+            ContainerSpawn::Pickup(spawn) => spawn_pickup(
+                &mut commands,
+                &mut assets_sprite,
+                Vec2::ZERO,
+                &spawn.from_spawn(position.0, depth.clone()),
+            ),
+            ContainerSpawn::Enemy(spawn) => spawn_enemy(
+                &mut commands,
+                Vec2::ZERO,
+                &spawn.from_spawn(position.0, depth.clone()),
+            ),
         };
 
         for mut unhittable_list in &mut attack_query.iter_mut() {
