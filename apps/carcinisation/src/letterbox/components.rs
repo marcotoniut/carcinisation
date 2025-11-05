06256297537f676f714f9e9d65ca383efa18b647
Marco Toniut <marco.toniut@gmail.com>
2025-11-05 00:31:15 +0000

    fix a few compilation and runtime issues

--- apps/carcinisation/src/letterbox/components.rs
+++ apps/carcinisation/src/letterbox/components.rs
@@ -17,4 +17,4 @@ pub struct LetterboxTop;
 pub const LETTERBOX_NORMAL_SPEED: f32 = 10.;
 pub const LETTERBOX_INSTANT_SPEED: f32 = f32::MAX;
 
-pub const LETTERBOX_HEIGHT: u32 = 30;
+pub const LETTERBOX_HEIGHT: u32 = 20;
