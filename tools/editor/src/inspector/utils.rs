987b6c8644921f144192bc45d1d001e8beb9744c
Marco Toniut <marco.toniut@gmail.com>
2025-12-16 01:16:17 -0300

    Simplify enum clones with copy.

--- tools/editor/src/inspector/utils.rs
+++ tools/editor/src/inspector/utils.rs
@@ -94,15 +94,15 @@ impl StageSpawnUtils for StageSpawn {
         match self {
             StageSpawn::Destructible(DestructibleSpawn {
                 destructible_type, ..
-            }) => get_destructible_thumbnail(destructible_type.clone(), self.get_depth()),
+            }) => get_destructible_thumbnail(destructible_type, self.get_depth()),
             StageSpawn::Enemy(EnemySpawn { enemy_type, .. }) => {
-                get_enemy_thumbnail(enemy_type.clone(), self.get_depth())
+                get_enemy_thumbnail(enemy_type, self.get_depth())
             }
             StageSpawn::Object(ObjectSpawn { object_type, .. }) => {
-                get_object_thumbnail(object_type.clone(), self.get_depth())
+                get_object_thumbnail(object_type, self.get_depth())
             }
             StageSpawn::Pickup(PickupSpawn { pickup_type, .. }) => {
-                get_pickup_thumbnail(pickup_type.clone(), self.get_depth())
+                get_pickup_thumbnail(pickup_type, self.get_depth())
             }
         }
     }
