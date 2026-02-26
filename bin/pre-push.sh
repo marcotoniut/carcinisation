a79c5eaa4de8d6f95a34d30ce1449930a073d767
Marco Toniut <marco.toniut@gmail.com>
2026-02-26 01:16:25 +0000

    chore: install and setup aesprite mcp

--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -11,7 +11,7 @@ run_check() {
 
 run_check cargo fmt --all -- --check
 run_check pnpm lint
-run_check proto run ruff -- check
+run_check proto run ruff -- check scripts
 run_check cargo clippy --workspace --all-targets --all-features -- -D warnings
 run_check make lint
 CARGO_TERM_PROGRESS_WHEN=always CARGO_TERM_PROGRESS_WIDTH=60 run_check make test
